8ef865ce06394d7186240350d468bf44b76c575d
abelperezd <abelperdan@gmail.com>
2023-02-22 19:17:00 +0100

    Links added

--- S01T07PerezDanesAbel/src/n1e2/Main.java
+++ S01T07PerezDanesAbel/src/n1e2/Main.java
@@ -1,5 +1,11 @@
 package n1e2;
 
+//Good video about annotations
+//https://www.youtube.com/watch?v=DkZr7_c9ry8
+
+//About serialization:
+//https://www.baeldung.com/java-custom-annotation
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,7 +17,8 @@ public class Main {
         System.out.println("Sou treballador online: " + treballadorOnline.calcularSou(5));
         System.out.println("Sou treballador presencial: " + treballadorPresencial.calcularSou(5));
 
-        System.out.println("Sou diari treballador online: " + treballadorOnline.calcularSouDiari(2));
+        
+        System.out.println("Sou diari treballador online: " +  treballadorOnline.calcularSouDiari(2));
         System.out.println("Sou diari treballador presencial: " + treballadorPresencial.calcularSouDiari(2));
     }
 }
