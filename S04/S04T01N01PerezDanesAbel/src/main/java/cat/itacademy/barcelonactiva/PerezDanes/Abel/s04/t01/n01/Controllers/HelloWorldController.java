4f5bbc0badadc2023b990e823e45910a35790ec5
abelperezd <abelperdan@gmail.com>
2023-05-14 17:03:29 +0200

    n1 and n2 done

---
+++ S04/S04T01N01PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s04/t01/n01/Controllers/HelloWorldController.java
@@ -0,0 +1,26 @@
+package cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t01.n01.Controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// https://youtu.be/9SGDpanrc8U?t=972
+// https://www.baeldung.com/spring-pathvariable
+@SpringBootApplication
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom) {
+        return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+    }
+
+    @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(value = "nom", required = false) String nom) {
+        nom = nom != null ? nom : "UNKNOWN";
+        return "Hola, " + (nom) + ". Estàs executant un projecte Maven.";
+    }
+
+}
---
+++ S04/S04T01N01PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s04/t01/n01/S04T01N01PerezDanesAbelApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01PerezDanesAbelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01PerezDanesAbelApplication.class, args);
+	}
+
+}
