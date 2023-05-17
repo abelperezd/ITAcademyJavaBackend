1ae15bfa5b1230364d598e660ddf4953e9328108
abelperezd <abelperdan@gmail.com>
2023-05-17 19:38:44 +0200

    n2 with service package

--- S04/S04T02N02PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s04/t02/n02/controller/FruitaController.java
+++ S04/S04T02N02PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s04/t02/n02/controller/FruitaController.java
@@ -2,6 +2,7 @@ package cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t02.n02.controller;
 
 
 import cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t02.n02.model.Fruita;
+import cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t02.n02.services.FruitaService;
 import org.springframework.http.HttpStatus;
 import cat.itacademy.barcelonactiva.PerezDanes.Abel.s04.t02.n02.repository.FruitaRepository;
 
@@ -19,87 +20,46 @@ import java.util.Optional;
 public class FruitaController {
 
     @Autowired
-    FruitaRepository fruitaRepository;
+    private final FruitaService fruitaService;
+
+    public FruitaController(FruitaService fruitaService) {
+        this.fruitaService = fruitaService;
+    }
 
     // http://localhost:8080/fruita/add => post
     @PostMapping("/add")
     public ResponseEntity<Fruita> createFruita(@RequestBody Fruita fruita) {
-        try {
-            Fruita _fruita = fruitaRepository
-                    .save(new Fruita(fruita.getNom(), fruita.getQuantitatQuilos()));
-            return new ResponseEntity<>(_fruita, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return fruitaService.createFruita(fruita);
     }
 
     //http://localhost:8080/fruita/update/{id} => put
     @PutMapping("/update/{id}")
     public ResponseEntity<Fruita> updateFruita(@PathVariable("id") long id, @RequestBody Fruita fruita) {
-        Optional<Fruita> fruitaData = fruitaRepository.findById(id);
-
-        if (fruitaData.isPresent()) {
-            Fruita _fruita = fruitaData.get();
-            if (!fruita.getNom().isEmpty())
-                _fruita.setNom(fruita.getNom());
-            if (fruita.getQuantitatQuilos() != 0)
-                _fruita.setQuantitatQuilos(fruita.getQuantitatQuilos());
-            return new ResponseEntity<>(fruitaRepository.save(_fruita), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return fruitaService.updateFruita(id, fruita);
     }
 
     //http://localhost:8080/fruita/delete/{id} => delete
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<HttpStatus> deleteFruita(@PathVariable("id") long id) {
-        try {
-            fruitaRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return fruitaService.deleteFruita(id);
     }
 
+    //http://localhost:8080/fruita/deleteAll => delete
     @DeleteMapping("/deleteAll")
     public ResponseEntity<HttpStatus> deleteAllFruitas() {
-        try {
-            fruitaRepository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        return fruitaService.deleteAllFruitas();
     }
 
-    //http://localhost:8080/fruita/getOne/{id}
+    //http://localhost:8080/fruita/getOne/{id} => get
     @GetMapping("/getOne/{id}")
     public ResponseEntity<Fruita> getOne(@PathVariable("id") long id) {
-        Optional<Fruita> fruitaData = fruitaRepository.findById(id);
-
-        if (fruitaData.isPresent()) {
-            return new ResponseEntity<>(fruitaData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return fruitaService.getOne(id);
     }
 
-
-    //http://localhost:8080/fruita/getAll
+    //http://localhost:8080/fruita/getAll => get
     @GetMapping("/getAll")
     public ResponseEntity<List<Fruita>> getAllFruitas() {
-        try {
-            List<Fruita> fruites = new ArrayList<>();
-
-            fruitaRepository.findAll().forEach(fruites::add);
-
-            if (fruites.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(fruites, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return fruitaService.getAllFruitas();
     }
 }
 
