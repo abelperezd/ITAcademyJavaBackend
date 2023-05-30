9c5803f2e84554fcc673bb46486866562af07296
abelperezd <abelperdan@gmail.com>
2023-05-30 15:32:06 +0200

    comments

--- S05/S05T01N01PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s05/t01/n01/S05T01N01PerezDanesAbelApplication.java
+++ S05/S05T01N01PerezDanesAbel/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s05/t01/n01/S05T01N01PerezDanesAbelApplication.java
@@ -3,6 +3,11 @@ package cat.itacademy.barcelonactiva.PerezDanes.Abel.s05.t01.n01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+https://www.youtube.com/watch?v=Ymv2e4-L1rc&list=PLxxZ0339925GvBiF39ieLkMjyvMlqihB2&index=1
+https://www.youtube.com/watch?v=9SGDpanrc8U
+*/
+
 @SpringBootApplication
 public class S05T01N01PerezDanesAbelApplication {
 
--- S05/S05T02N01PerezDanesAbelMongoDB/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s05/t02/n01/S05T02N01PerezDanesAbelApplication.java
+++ S05/S05T02N01PerezDanesAbelMongoDB/src/main/java/cat/itacademy/barcelonactiva/PerezDanes/Abel/s05/t02/n01/S05T02N01PerezDanesAbelApplication.java
@@ -3,8 +3,12 @@ package cat.itacademy.barcelonactiva.PerezDanes.Abel.s05.t02.n01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+https://www.mongodb.com/compatibility/spring-boot
+*/
+
 @SpringBootApplication
-public class S05T02N01PerezDanesAbelApplication {
+public class  S05T02N01PerezDanesAbelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(S05T02N01PerezDanesAbelApplication.class, args);
